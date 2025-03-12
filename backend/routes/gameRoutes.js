0d86c9598a28ad5bb1e883fe3af05a900af1ee9a
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-12 15:51:13 +0530

    Automated commit: backend/routes/gameRoutes.js

---
+++ backend/routes/gameRoutes.js
@@ -0,0 +1,1 @@
+const express = require('express');const router = express.Router();const gameController = require('../controllers/gameController');router.post('/', gameController.createGame);router.get('/:id', gameController.getGame);router.put('/:id', gameController.updateGame);module.exports = router;
