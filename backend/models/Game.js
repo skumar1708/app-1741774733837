bf71d2173b6397cd802f5dcf58c84334cbc9a66b
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-12 15:51:02 +0530

    Automated commit: backend/models/Game.js

---
+++ backend/models/Game.js
@@ -0,0 +1,1 @@
+const mongoose = require('mongoose');const gameSchema = new mongoose.Schema({board: {type: Array,default: Array(9).fill(null),},currentPlayer: {type: String,default: 'X',},winner: {type: String,default: null,},}, {timestamps: true});module.exports = mongoose.model('Game', gameSchema);
